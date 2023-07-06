663d97e48a04f9031b07b72e67432d168419d17f
Tmoney$ <15959707+tpmiller87@users.noreply.github.com>
2023-07-06 11:38:44 -0400

    Update imager.sh
    Added newest cme release, made the script require sudo to run (and checks), and made the script more portable by having user define who owns the folder once finished.

--- imager.sh
+++ imager.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
+
+if [[ $EUID -ne 0 ]]; then
+    echo "You must run this script as root!"
+    exit 1
+fi
+
+read -p "What non-root user should own these tools?:  " user
+
+echo "$user will own the directory 'my_tools' and everything in it."
+echo "Exit this script if this is not what you want!"
+sleep 5
 echo "Starting the preferred tools startup script..."
 sleep 2
 #update repos
 
-sudo apt update
+apt update
 
 mkdir my_tools
-chown kali:kali my_tools
 cd my_tools
 
 #chrome install
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i *chrome*
+dpkg -i *chrome*
 rm *chrome*
 
 #name that hash install
@@ -24,13 +34,15 @@ apt install golang-go -y
 #SharpCollection install
 git clone https://github.com/Flangvik/SharpCollection.git
 
+#crackmapexec update - latest 6.0.0 "BANE"
+wget https://github.com/mpgn/CrackMapExec/releases/download/v6.0.0/cme-ubuntu-latest-3.11.zip
+unzip cme-ubuntu-latest-3.11.zip
+chmod +x cme
+cp cme /usr/bin 
+
 #make permissions all for user 'kali' from root
-chown kali:kali *
+cd -
+chown -R $user:$user my_tools
 
 #to-do:
-#eyewitness
-#
-#
-#
-#
-#
+#TBD
